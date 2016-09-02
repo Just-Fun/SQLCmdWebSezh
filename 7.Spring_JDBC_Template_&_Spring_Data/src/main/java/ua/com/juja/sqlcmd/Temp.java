d2d77030c55cb25966e8c7a3f3e9448f56584c34
Just-Fun <Just-Fun@users.noreply.github.com>
2016-09-02 17:25:17 +0300

    добавил команды sql

---
+++ 7.Spring_JDBC_Template_&_Spring_Data/src/main/java/ua/com/juja/sqlcmd/Temp.java
@@ -0,0 +1,45 @@
+package ua.com.juja.sqlcmd;
+
+/**
+ * Created by Serzh on 9/2/16.
+ */
+public class Temp {
+
+
+    /*- Database: sqlcmd_log
+
+-- DROP DATABASE sqlcmd_log;
+
+CREATE DATABASE sqlcmd_log
+  WITH OWNER = postgres
+       ENCODING = 'UTF8'
+       TABLESPACE = pg_default
+       LC_COLLATE = 'C'
+       LC_CTYPE = 'C'
+       CONNECTION LIMIT = -1;
+*/
+
+
+    /*
+    CREATE TABLE user_actions (id SERIAL NOT NULL PRIMARY KEY, user_name varchar(225) NOT NULL UNIQUE,
+    db_name varchar(225) NOT NULL, action varchar(225) NOT NULL)*/
+
+    /*-- Table: public.user_actions
+
+-- DROP TABLE public.user_actions;
+
+CREATE TABLE public.user_actions
+(
+  id integer NOT NULL DEFAULT nextval('user_actions_id_seq'::regclass),
+  user_name character varying(225) NOT NULL,
+  db_name character varying(225) NOT NULL,
+  action character varying(225) NOT NULL,
+  CONSTRAINT user_actions_pkey PRIMARY KEY (id),
+  CONSTRAINT user_actions_user_name_key UNIQUE (user_name)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE public.user_actions
+  OWNER TO postgres;*/
+}
